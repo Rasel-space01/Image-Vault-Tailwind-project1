aba7c76669b940445f0a35c3b655e027f4b52a6b
rasel-space01 <raselhossain0063@gmail.com>
2023-12-10 19:41:39 +0600

    My commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+    colors:{
+      "top-nav":"#000000",
+      "sub-nav":"#353131",
+      "f-color":"#FFFFFF",
+      "main-bg":"#bcb88a",
+      "ser-color":"#FFFFFF"
+    }
+  },
+  plugins: [],
+}
+
